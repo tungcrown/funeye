0ab6c51617f5aa14149e77a70b4e71cdd00a5dc3
Lê Thanh Tùng <lethanhtung@tungs-macbook-pro.local>
2016-03-22 09:46:55 +0700

    Initial Commit

---
+++ FunEye1.2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunEye1.2
+//
+//  Created by Lê Thanh Tùng on 3/22/16.
+//  Copyright © 2016 Lê Thanh Tùng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
